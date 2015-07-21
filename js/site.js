7387f5083e971214e8fccf35ad5b7cd46b8fb724
Joe Ellis <joe.ellis@avcosystems.com>
2015-07-21 22:28:28 +0100

    Hinge lol

--- js/site.js
+++ js/site.js
@@ -1,23 +1,33 @@
 $(document).ready(function(){
     $('.ig-link').hover(function (){
-        ToggleAnimation($('.ig-link'));
+        ToggleTadaAnimation($('.ig-link'));
     })
 
     $('.li-link').hover(function (){
-        ToggleAnimation($('.li-link'));
+        ToggleTadaAnimation($('.li-link'));
     })
 
     $('.fb-link').hover(function (){
-        ToggleAnimation($('.fb-link'));
+        ToggleTadaAnimation($('.fb-link'));
     })
 
     $('.sc-link').hover(function (){
-        ToggleAnimation($('.sc-link'));
+        ToggleTadaAnimation($('.sc-link'));
+    })
+
+    $('.photo').hover(function (){
+        ToggleBounceAnimation($('.photo'));
     })
 });
 
-function ToggleAnimation(element)
+function ToggleTadaAnimation(element)
 {
     element.toggleClass('animated');
     element.toggleClass('tada');
+}
+
+function ToggleBounceAnimation(element)
+{
+    element.toggleClass('animated');
+    element.toggleClass('hinge');
 }
