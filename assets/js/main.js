ac6fb60e0d1bba81b9fd445219611a2566a0ccb7
Joe Ellis <joe.ellis@avcosystems.com>
2015-11-12 23:28:28 +0000

    Animated the bio drop down

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
 	$('.tooltips').tooltipster();
+
+	$("#me").click(function (){
+		$(".info-content").addClass('animate-in');
+	})
+
+	$("#skills").click(function (){
+
+	})
+
+	$("#career").click(function (){
+
+	})
+
+	$("#education").click(function (){
+
+	})
 });
