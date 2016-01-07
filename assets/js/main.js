040f859d4884aa5b62685744e38fff3dda2eee6c
Joe Ellis <joe.ellis@avcosystems.com>
2016-01-07 14:04:34 +0000

    Yep, you can render HTML. Awesome.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,30 @@ $(document).ready(function() {
 	$('.tooltips').tooltipster();
 
 	$("#me").on("click", function() {
-		bootbox.alert("Hello World!");
+		bootbox.dialog({
+		  title: "That html",
+		  message: '<img src="images/bootstrap_logo.png" width="100px"/><br/> You can also use <b>html</b>'
+		});
 	});
 
 	$("#skills").on("click", function() {
-		bootbox.alert("Hello World!");
+		bootbox.dialog({
+		  title: "That html",
+		  message: '<img src="images/bootstrap_logo.png" width="100px"/><br/> You can also use <b>html</b>'
+		});
 	});
 
 	$("#career").on("click", function() {
-		bootbox.alert("Hello World!");
+		bootbox.dialog({
+		  title: "That html",
+		  message: '<img src="images/bootstrap_logo.png" width="100px"/><br/> You can also use <b>html</b>'
+		});
 	});
 
 	$("#education").on("click", function() {
-		bootbox.alert("Hello World!");
+		bootbox.dialog({
+		  title: "That html",
+		  message: '<img src="images/bootstrap_logo.png" width="100px"/><br/> You can also use <b>html</b>'
+		});
 	});
 });
