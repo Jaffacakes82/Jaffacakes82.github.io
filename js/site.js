7545b79735537efb5c4a65361637fba6000aa13f
Joseph Ellis <joseph.ellis@prosperityfundfco.onmicrosoft.com>
2018-03-21 08:46:55 +0000

    Moved hawking quotes UI into github pages

---
+++ js/site.js
@@ -0,0 +1,20 @@
+﻿// Write your JavaScript code.
+$(document).ready(function () {
+    $(".hawking").click(function () {
+        $(".hawking").addClass('wobble');
+        setTimeout(function () {
+            $(".hawking").removeClass('wobble');
+        }, 1500);
+
+        $.get('http://hawkingquotesapi.azurewebsites.net/api/quote', function (data) {
+            $(".speech-bubble > p").text('"' + data + '"');
+        })
+
+        $(".speech-bubble").show();
+        $(".speech-bubble").addClass('lightSpeedIn');
+
+        setTimeout(function () {
+            $(".speech-bubble").removeClass('lightSpeedIn');
+        }, 1500);
+    });
+});
